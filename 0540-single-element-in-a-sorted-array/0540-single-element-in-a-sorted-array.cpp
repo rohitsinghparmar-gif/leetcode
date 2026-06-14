2174f9091fb17151683fef65fc070ac067bfcc9d
rohitsinghparmar-gif <parmarrohitsingh4@gmail.com>
2026-06-14 12:54:38 +0530

    Time: 0 ms (100%), Space: 26.3 MB (33.59%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        for(int num:nums){
-            mp[num]++;
+        // unordered_map<int,int>mp;
+        // for(int num:nums){
+        //     mp[num]++;
+        // }
+        // for(auto it:mp){
+        //     if(it.second==1){
+        //         return it.first;
+        //     }
+        // }
+        // return -1;
+        int result=0;
+        for(int i=0;i<nums.size();i++){
+            result=result^nums[i];
+
         }
-        for(auto it:mp){
-            if(it.second==1){
-                return it.first;
-            }
-        }
-        return -1;
-        
+        return result;
     }
 };
