a54b67f2c043b6982f289f83ac6626e87e8a9a89
rohitsinghparmar-gif <parmarrohitsingh4@gmail.com>
2026-07-30 22:51:27 +0530

    Time: 0 ms (100%), Space: 8.1 MB (95.83%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     int minimumPushes(string word) {
          int n=word.size();
-         if(n<=8){
-            return n;
-         }   
-         if(n>8 && n<=16){
-            return 8+(n-8)*2;
-         }  
-         if(n>16 && n<=24){
-            return 24+(n-16)*3;
-         }  
-         return  48+(n-24)*4; 
+        //  if(n<=8){
+        //     return n;
+        //  }   
+        //  if(n>8 && n<=16){
+        //     return 8+(n-8)*2;
+        //  }  
+        //  if(n>16 && n<=24){
+        //     return 24+(n-16)*3;
+        //  }  
+        //  return  48+(n-24)*4; 
+        return n+max((n-8),0)+max((n-16),0)+max((n-24),0);
     }
 };
