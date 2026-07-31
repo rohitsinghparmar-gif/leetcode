0a7e3e5e54ac9b3fc46962ec806bd9f97ae21a4f
rohitsinghparmar-gif <parmarrohitsingh4@gmail.com>
2026-07-31 22:50:50 +0530

    Time: 13 ms (38.94%), Space: 25.4 MB (72.47%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,3 +1,32 @@
+// class Solution {
+// public:
+//     int minimumPushes(string word) {
+//         int n=word.size();
+//         vector<int>freq(26,0);
+//         for(char ch:word){
+//             freq[ch-'a']++;
+//         }
+//         sort(freq.begin(),freq.end(),greater<int>());
+//         int sum=0;
+//         for(int i=0;i<freq.size();i++){
+//             if(i<8){
+//                 sum=sum+freq[i];
+//             }
+//              else if(i<16){
+//                 sum=sum+freq[i]*2;
+//             }
+//              else if(i <24){
+//                 sum=sum+freq[i]*3;
+//             }
+//             else{
+//                 sum=sum+freq[i]*4;
+//             }
+//         }
+//         return sum;
+
+        
+//     }
+// };
 class Solution {
 public:
     int minimumPushes(string word) {
@@ -12,10 +41,10 @@ public:
             if(i<8){
                 sum=sum+freq[i];
             }
-             else if(i<16){
+             else if(i>=8&&i<16){
                 sum=sum+freq[i]*2;
             }
-             else if(i <24){
+             else if(i>=16 &&i <24){
                 sum=sum+freq[i]*3;
             }
             else{
